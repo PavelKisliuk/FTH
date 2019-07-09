222224c8e38bc5797b67afbdb7c97459459f381c
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-07-09 13:14:12 +0300

    FthData commented

---
+++ src/main/java/com/pavelkisliuk/fth/model/FthData.java
@@ -0,0 +1,16 @@
+/*  By Pavel Kisliuk, 07.07.2019
+ *  This is class for education and nothing rights don't reserved.
+ */
+
+package com.pavelkisliuk.fth.model;
+
+/**
+ * Interface for unification of reference data types, which are used by {@code FthRepository}.
+ * <p>
+ *
+ * @author Kisliuk Pavel Sergeevich
+ * @see com.pavelkisliuk.fth.repository.FthRepository
+ * @since 12.0
+ */
+public interface FthData {
+}
