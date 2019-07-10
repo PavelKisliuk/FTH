db141cce7cc19594eb4e4138e2741f541b4f88f9
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-07-10 16:49:06 +0300

    Add FthLong FthLong Commented

---
+++ src/main/java/com/pavelkisliuk/fth/model/FthLong.java
@@ -0,0 +1,69 @@
+/*  By Pavel Kisliuk, 10.07.2019
+ *  This is class for education and nothing rights don't reserved.
+ */
+
+package com.pavelkisliuk.fth.model;
+
+/**
+ * The class {@code FthLong} is class-wrapper for {@code Long}.
+ * <p>
+ *
+ * @author Kisliuk Pavel Sergeevich
+ * @see FthData
+ * @since 12.0
+ */
+public class FthLong implements FthData {
+	/**
+	 * Wrapped variable.
+	 */
+	private long bigInt; //примитив или класс?
+
+	/**
+	 * Constructor for initialization.
+	 *
+	 * @param bigInt is variable for initialization {@code FthLong}.
+	 */
+	public FthLong(long bigInt) {
+		this.bigInt = bigInt;
+	}
+
+	/**
+	 * Getter for {@code FthLong}.
+	 *
+	 * @return {@code bigInt}.
+	 */
+	public long get() {
+		return bigInt;
+	}
+
+	/**
+	 * Setter for {@code FthLong}.
+	 *
+	 * @param bigInt is variable for setting.
+	 */
+	public void set(long bigInt) {
+		this.bigInt = bigInt;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		FthLong fthLong = (FthLong) o;
+
+		return bigInt == fthLong.bigInt;
+	}
+
+	@Override
+	public int hashCode() {
+		return (int) (bigInt ^ (bigInt >>> 32));
+	}
+
+	@Override
+	public String toString() {
+		return "FthLong{" +
+				"bigInt=" + bigInt +
+				'}';
+	}
+}
