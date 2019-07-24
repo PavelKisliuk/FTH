8f45a7ae9cd2c83bba3cd2782b7269b8bd41a70f
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-07-24 23:12:50 +0300

    Add AllAuthenticationEmailSelectSpecifier AllAuthenticationEmailSelectSpecifier commented

---
+++ src/main/java/com/pavelkisliuk/fth/specifier/select/AllAuthenticationEmailSelectSpecifier.java
@@ -0,0 +1,60 @@
+/*  By Pavel Kisliuk, 24.07.2019
+ *  This is class for education and nothing rights don't reserved.
+ */
+
+package com.pavelkisliuk.fth.specifier.select;
+
+import com.pavelkisliuk.fth.repository.FthDataByResultSetFactory;
+import com.pavelkisliuk.fth.specifier.FthSelectSpecifier;
+
+import java.sql.PreparedStatement;
+
+/**
+ * The {@code AllAuthenticationEmailSelectSpecifier} class is {@code FthSelectSpecifier} realization
+ * for selection all e-mail from table AuthenticationData in database.
+ * <p>
+ *
+ * @author Kisliuk Pavel Sergeevich
+ * @since 12.0
+ */
+public class AllAuthenticationEmailSelectSpecifier implements FthSelectSpecifier {
+	/**
+	 * Select request to database.
+	 */
+	private static final String REQUEST = "SELECT " +
+			"eMail " +
+			"FROM AuthenticationData";
+
+	/**
+	 * Return factory for {@code FthString} creation.
+	 * <p>
+	 *
+	 * @return factory for {@code FthString} creation.
+	 */
+	@Override
+	public FthDataByResultSetFactory createFactory() {
+		return new CreatorString();
+	}
+
+	/**
+	 * Paste metadata in {@param statement} and return it.
+	 *
+	 * @param statement for pasting metadata into.
+	 * @return {@param statement}.
+	 */
+	@Override
+	public PreparedStatement pasteMeta(PreparedStatement statement) {
+		return statement;
+	}
+
+	/**
+	 * Return {@code REQUEST}.
+	 * <p>
+	 *
+	 * @return {@code REQUEST}.
+	 */
+	@Override
+	public String deriveSequelRequest() {
+		return REQUEST;
+	}
+}
