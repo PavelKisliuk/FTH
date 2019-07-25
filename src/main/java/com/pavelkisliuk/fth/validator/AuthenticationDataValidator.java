beb35f90138415c23141ec149ad80144f148229b
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-07-25 09:07:01 +0300

    Add AuthenticationDataValidator AuthenticationDataValidator commented

--- src/main/java/com/pavelkisliuk/fth/validator/AuthenticationDataValidator.java
+++ src/main/java/com/pavelkisliuk/fth/validator/AuthenticationDataValidator.java
@@ -1,3 +1,7 @@
+/*  By Pavel Kisliuk, 11.07.2019
+ *  This is class for education and nothing rights don't reserved.
+ */
+
 package com.pavelkisliuk.fth.validator;
 
 import com.pavelkisliuk.fth.exception.FthControllerException;
@@ -6,34 +10,68 @@ import com.pavelkisliuk.fth.model.FthAuthenticationData;
 import com.pavelkisliuk.fth.model.FthData;
 import com.pavelkisliuk.fth.model.FthInt;
 import com.pavelkisliuk.fth.repository.FthRepository;
-import com.pavelkisliuk.fth.specifier.select.AuthenticateSpecifier;
+import com.pavelkisliuk.fth.specifier.select.AuthenticateSelectSpecifier;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 
+/**
+ * The {@code AuthenticationDataValidator} class is {@code FthValidator} realization for
+ * validation {@code FthAuthenticationData}. Validator check e-mail and password of trainer,
+ * if this trainer exist in system.
+ * <p>
+ *
+ * @author Kisliuk Pavel Sergeevich
+ * @since 12.0
+ */
 public class AuthenticationDataValidator implements FthValidator {
+	private static final Logger LOGGER = LogManager.getLogger();
+
 	private static final String EMAIL_EMPTY = "Field \"email\" empty.\n";
 	private static final String PASSWORD_EMPTY = "Field \"password\" empty.\n";
 
-	private static final String ICORRECT = "Incorrect e-mail or password.";
+	private static final String INCORRECT = "Incorrect e-mail or password.";
 	/**
-	 * {@code ArrayList} for messages about incorrect data.
+	 * Messages about incorrect data.
 	 */
 	private ArrayList<String> messageGroup;
 
+	/**
+	 * Inspect {@code FthAuthenticationData} instance for correct data.
+	 * <p>
+	 *
+	 * @param data is data for validation.
+	 * @return {@code true} if {@param data} valid, else return {@code false}.
+	 */
 	@Override
 	public boolean isCorrect(FthData data) throws FthControllerException {
+		LOGGER.log(Level.DEBUG,
+				"Start AuthenticationDataValidator -> isCorrect(FthData).");
+		if (data == null ||
+				data.getClass() != FthAuthenticationData.class) {
+			LOGGER.log(Level.ERROR,
+					"Incorrect parameter in AuthenticationDataValidator -> isCorrect(FthData)!!!");
+			return false;
+		}
+
 		messageGroup = new ArrayList<>();
 		FthAuthenticationData authenticationData = (FthAuthenticationData) data;
-		return isDataNotEmpty(authenticationData) && isExist(authenticationData);
+		return isBlank(authenticationData) && isExist(authenticationData);
 	}
 
-	private boolean isDataNotEmpty(FthAuthenticationData authenticationData) {
+	private boolean isBlank(FthAuthenticationData authenticationData) {
 		boolean flag = true;
 		if (authenticationData.getEmail() == null || authenticationData.getEmail().isBlank()) {
+			LOGGER.log(Level.WARN,
+					"E-mail empty!");
 			flag = false;
 			messageGroup.add(EMAIL_EMPTY);
 		}
 		if (authenticationData.getPassword() == null || authenticationData.getPassword().isBlank()) {
+			LOGGER.log(Level.WARN,
+					"Password empty!");
 			flag = false;
 			messageGroup.add(PASSWORD_EMPTY);
 		}
@@ -43,9 +81,12 @@ public class AuthenticationDataValidator implements FthValidator {
 	private boolean isExist(FthAuthenticationData authenticationData) throws FthControllerException {
 		boolean flag = true;
 		try {
-			FthInt fthInt = (FthInt) FthRepository.INSTANCE.query(new AuthenticateSpecifier(authenticationData)).get(0);
+			FthInt fthInt = (FthInt) FthRepository.INSTANCE.query(
+					new AuthenticateSelectSpecifier(authenticationData)).get(0);
 			if(fthInt.get() != 1) {
-				messageGroup.add(ICORRECT);
+				LOGGER.log(Level.WARN,
+						"Trainer not exist in system!");
+				messageGroup.add(INCORRECT);
 				flag = false;
 			}
 		} catch (FthRepositoryException e) {
