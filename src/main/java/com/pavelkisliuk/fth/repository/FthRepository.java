d2b0475b8c0f020665945ac65e9e8b1edbd063d1
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-08-06 13:34:42 +0300

    FthRepository refactor (transactions, rollback, setautocommit to pool)

--- src/main/java/com/pavelkisliuk/fth/repository/FthRepository.java
+++ src/main/java/com/pavelkisliuk/fth/repository/FthRepository.java
@@ -11,6 +11,8 @@ import com.pavelkisliuk.fth.pool.ConnectionPool;
 import com.pavelkisliuk.fth.specifier.FthDeleteSpecifier;
 import com.pavelkisliuk.fth.specifier.FthInsertSpecifier;
 import com.pavelkisliuk.fth.specifier.FthSelectSpecifier;
+import com.pavelkisliuk.fth.specifier.FthSpecifier;
+import com.pavelkisliuk.fth.specifier.FthUpdateSpecifier;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -19,7 +21,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,6 +43,32 @@ public enum FthRepository {
 	private static final Logger LOGGER = LogManager.getLogger();
 
 	/**
+	 * Timeout for waiting connection from pool in millisecond.
+	 */
+	private static final int CONNECTION_WAITING_INTERVAL = 5000;
+
+	/**
+	 * The {@code TransactionOperationType} class enum describing actions with database in transactions.
+	 */
+	public enum TransactionOperationType {
+		INSERT, UPDATE, DELETE
+	}
+
+	/**
+	 * The {@code TransactionDescriber} class specify operation and specifier for this operation.
+	 * Used for transaction actions.
+	 */
+	public static class TransactionDescriber {
+		private TransactionOperationType operationType;
+		private FthSpecifier specifier;
+
+		public TransactionDescriber(TransactionOperationType operationType, FthSpecifier specifier) {
+			this.operationType = operationType;
+			this.specifier = specifier;
+		}
+	}
+
+	/**
 	 * Insert data into database.
 	 * <p>
 	 *
@@ -47,23 +76,69 @@ public enum FthRepository {
 	 * @throws FthRepositoryException if {@code SQLException}, {@code FthSpecifierException} occurred.
 	 */
 	public void add(FthInsertSpecifier specifier) throws FthRepositoryException {
-		LOGGER.log(Level.DEBUG, "Start FthRepository -> add().");
+		LOGGER.log(Level.DEBUG,
+				"Start FthRepository -> add(FthInsertSpecifier).");
+		if (specifier == null) {
+			throw new FthRepositoryException(
+					"null parameter in FthRepository -> add(FthInsertSpecifier).");
+		}
+
 		add(specifier, obtainConnection());
-		LOGGER.log(Level.DEBUG, "Finish FthRepository -> add().");
+		LOGGER.log(Level.DEBUG,
+				"Finish FthRepository -> add(FthInsertSpecifier).");
 	}
 
 	private void add(FthInsertSpecifier specifier, Connection connection) throws FthRepositoryException {
 		try (PreparedStatement statement = connection.prepareStatement(specifier.deriveSequelRequest())) {
+			LOGGER.log(Level.INFO,
+					"Insertion data.");
 			specifier.insert(statement);
-			LOGGER.log(Level.DEBUG, "Insertion achieved.");
+			LOGGER.log(Level.INFO,
+					"Insertion achieved.");
 		} catch (SQLException e) {
-			throw new FthRepositoryException("Problem in add() method!", e);
+			throw new FthRepositoryException(
+					"SQLException in FthRepository -> add(FthInsertSpecifier, Connection).", e);
 		} finally {
-			try {
-				ConnectionPool.INSTANCE.releaseConnection(connection);
-			} catch (ConnectionPoolException e) {
-				LOGGER.log(Level.ERROR, "Damaged connection tried to realise!", e);
-			}
+			ConnectionPool.INSTANCE.releaseConnection(connection);
+			LOGGER.log(Level.INFO,
+					"Connection released.");
+		}
+	}
+
+	/**
+	 * Update data into database.
+	 * <p>
+	 *
+	 * @param specifier for obtaining special actions.
+	 * @throws FthRepositoryException if {@code SQLException} occurred.
+	 */
+	public void replace(FthUpdateSpecifier specifier) throws FthRepositoryException {
+		LOGGER.log(Level.DEBUG,
+				"Start FthRepository -> replace(FthUpdateSpecifier).");
+		if (specifier == null) {
+			throw new FthRepositoryException(
+					"null parameter in FthRepository -> replace(FthUpdateSpecifier).");
+		}
+
+		replace(specifier, obtainConnection());
+		LOGGER.log(Level.DEBUG,
+				"Finish FthRepository -> replace(FthUpdateSpecifier).");
+	}
+
+	private void replace(FthUpdateSpecifier specifier, Connection connection) throws FthRepositoryException {
+		try (PreparedStatement statement = connection.prepareStatement(specifier.deriveSequelRequest())) {
+			LOGGER.log(Level.INFO,
+					"Updating data.");
+			specifier.update(statement);
+			LOGGER.log(Level.INFO,
+					"Updating achieved.");
+		} catch (SQLException e) {
+			throw new FthRepositoryException(
+					"SQLException in FthRepository -> replace(FthUpdateSpecifier, Connection).", e);
+		} finally {
+			ConnectionPool.INSTANCE.releaseConnection(connection);
+			LOGGER.log(Level.INFO,
+					"Connection released.");
 		}
 	}
 
@@ -75,25 +150,76 @@ public enum FthRepository {
 	 * @throws FthRepositoryException if {@code SQLException}, {@code FthSpecifierException} occurred.
 	 */
 	public void remove(FthDeleteSpecifier specifier) throws FthRepositoryException {
-		LOGGER.log(Level.DEBUG, "Start FthRepository -> remove().");
-		Optional<Connection> connectionOptional;
-		do {
-			connectionOptional = ConnectionPool.INSTANCE.obtainConnection();
-		} while (connectionOptional.isEmpty());
-		Connection connection = connectionOptional.get();
-		try (PreparedStatement statement = connection.prepareStatement(specifier.deriveSequelRequest())) {
-			specifier.delete(statement);
-			LOGGER.log(Level.DEBUG, "Deletion achieved.");
-		} catch (SQLException e) {
-			throw new FthRepositoryException("Problem in remove() method!", e);
-		} finally {
-			try {
-				ConnectionPool.INSTANCE.releaseConnection(connection);
-			} catch (ConnectionPoolException e) {
-				LOGGER.log(Level.ERROR, "Damaged connection tried to realise!", e);
-			}
+		LOGGER.log(Level.DEBUG,
+				"Start FthRepository -> remove(FthDeleteSpecifier).");
+		if (specifier == null) {
+			throw new FthRepositoryException(
+					"null parameter in FthRepository -> remove(FthDeleteSpecifier).");
 		}
-		LOGGER.log(Level.DEBUG, "Finish FthRepository -> remove().");
+
+		remove(specifier, obtainConnection());
+		LOGGER.log(Level.DEBUG,
+				"Finish FthRepository -> remove(FthDeleteSpecifier).");
+	}
+
+	private void remove(FthDeleteSpecifier specifier, Connection connection) throws FthRepositoryException {
+		try (PreparedStatement statement = connection.prepareStatement(specifier.deriveSequelRequest())) {
+			LOGGER.log(Level.INFO,
+					"Deleting data.");
+			specifier.delete(statement);
+			LOGGER.log(Level.INFO,
+					"Deletion achieved.");
+		} catch (SQLException e) {
+			throw new FthRepositoryException(
+					"SQLException in FthRepository -> remove(FthDeleteSpecifier, Connection).", e);
+		} finally {
+			ConnectionPool.INSTANCE.releaseConnection(connection);
+			LOGGER.log(Level.INFO,
+					"Connection released.");
+		}
+	}
+
+	public void operateTransaction(List<TransactionDescriber> transactionDescriberList) throws FthRepositoryException {
+		if (transactionDescriberList == null) {
+			throw new FthRepositoryException(
+					"null parameter in FthRepository -> operateTransaction(List<TransactionDescriber>).");
+		}
+
+		Connection connection = obtainConnection();
+		try {
+			connection.setAutoCommit(false);
+			for (TransactionDescriber describer : transactionDescriberList) {
+				switch (describer.operationType) {
+					case INSERT:
+						add((FthInsertSpecifier) describer.specifier, connection);
+						break;
+					case UPDATE:
+						replace((FthUpdateSpecifier) describer.specifier, connection);
+						break;
+					case DELETE:
+						remove((FthDeleteSpecifier) describer.specifier, connection);
+						break;
+					default:
+						// FIXME: 06.08.2019 правильно ли?
+						throw new EnumConstantNotPresentException(TransactionOperationType.class,
+								"Not correct enum element in " +
+										"FthRepository -> operateTransaction(List<TransactionDescriber>).");
+				}
+			}
+			connection.commit();
+		} catch (SQLException e) {
+			try {
+				connection.rollback();
+			} catch (SQLException ex) {
+				// FIXME: 06.08.2019 отправить письмо админу
+				throw new FthRepositoryException(
+						"SQLException in FthRepository -> operateTransaction(List<TransactionDescriber>).", ex);
+			}
+			throw new FthRepositoryException(
+					"SQLException in FthRepository -> operateTransaction(List<TransactionDescriber>).", e);
+		} finally {
+			ConnectionPool.INSTANCE.releaseConnection(connection);
+		}
 	}
 
 	/**
@@ -102,41 +228,89 @@ public enum FthRepository {
 	 *
 	 * @param specifier for obtaining special actions.
 	 * @return {@code List} of {@code FthData}.
-	 * @throws FthRepositoryException if {@code SQLException}, {@code FthSpecifierException} occurred.
+	 * @throws FthRepositoryException if {@code SQLException} occurred.
 	 */
 	public List<FthData> query(FthSelectSpecifier specifier) throws FthRepositoryException {
-		LOGGER.log(Level.DEBUG, "Start FthRepository -> query().");
-		ArrayList<FthData> dataList = new ArrayList<>();
-		Optional<Connection> connectionOptional;
-		do {
-			connectionOptional = ConnectionPool.INSTANCE.obtainConnection();
-		} while (connectionOptional.isEmpty());
-		Connection connection = connectionOptional.get();
+		LOGGER.log(Level.DEBUG,
+				"Start FthRepository -> query(FthSelectSpecifier).");
+		if (specifier == null) {
+			throw new FthRepositoryException(
+					"null parameter in FthRepository -> query(FthSelectSpecifier).");
+		}
+
+		return query(specifier, obtainConnection());
+	}
+
+	private List<FthData> query(FthSelectSpecifier specifier, Connection connection) throws FthRepositoryException {
+		LinkedList<FthData> dataList = new LinkedList<>();
 		try (PreparedStatement statement = connection.prepareStatement(specifier.deriveSequelRequest());
 			 ResultSet resultSet = specifier.pasteMeta(statement).executeQuery()) {
 			FthDataByResultSetFactory factory = specifier.createFactory();
+			LOGGER.log(Level.INFO,
+					"Acquiring data.");
 			while (resultSet.next()) {
 				dataList.add(factory.create(resultSet));
 			}
-			LOGGER.log(Level.DEBUG, "Data acquired.");
+			LOGGER.log(Level.INFO,
+					"Data acquired.");
 		} catch (SQLException e) {
-			throw new FthRepositoryException("Problem in query() method!", e);
+			throw new FthRepositoryException(
+					"SQLException in FthRepository -> query(FthSelectSpecifier, Connection).", e);
 		} finally {
-			try {
-				ConnectionPool.INSTANCE.releaseConnection(connection);
-			} catch (ConnectionPoolException e) {
-				LOGGER.log(Level.ERROR, "Damaged connection tried to realise!", e);
-			}
+			ConnectionPool.INSTANCE.releaseConnection(connection);
+			LOGGER.log(Level.INFO,
+					"Connection released.");
 		}
-		LOGGER.log(Level.DEBUG, "Finish FthRepository -> query().");
+		LOGGER.log(Level.DEBUG,
+				"Finish FthRepository -> query(FthSelectSpecifier, Connection).");
 		return dataList;
 	}
 
-	private Connection obtainConnection() {
+	/**
+	 * Obtain connection from {@code ConnectionPool}.
+	 * <p>
+	 *
+	 * @return working {@code Connection} from {@code ConnectionPool}.
+	 * @throws FthRepositoryException if awaiting interval of {@code Connection} exceeded;
+	 *                                ConnectionPoolException occurred.
+	 */
+	private Connection obtainConnection() throws FthRepositoryException {
+		LOGGER.log(Level.DEBUG,
+				"Start FthRepository -> obtainConnection().");
 		Optional<Connection> connectionOptional;
+		Date date = new Date();
+		long expiredTime = date.getTime() + CONNECTION_WAITING_INTERVAL;
+		LOGGER.log(Level.INFO,
+				"Obtaining of connection.");
+		boolean firstEntry = true;
 		do {
-			connectionOptional = ConnectionPool.INSTANCE.obtainConnection();
+			try {
+				connectionOptional = ConnectionPool.INSTANCE.obtainConnection();
+			} catch (ConnectionPoolException e) {
+				throw new FthRepositoryException(
+						"ConnectionPoolException in FthRepository -> obtainConnection().", e);
+			}
+
+			if (firstEntry) {
+				firstEntry = false;
+			} else {
+				if (date.getTime() >= expiredTime) {
+					throw new FthRepositoryException(
+							"Too long awaiting of Connection.");
+				}
+				try {
+					int halfSecondMillis = 500;
+					Thread.sleep(halfSecondMillis);
+				} catch (InterruptedException e) {
+					LOGGER.log(Level.ERROR,
+							"Can't cease a thread!!!");
+				}
+			}
 		} while (connectionOptional.isEmpty());
+		LOGGER.log(Level.INFO,
+				"Connection obtained.");
+		LOGGER.log(Level.DEBUG,
+				"Finish FthRepository -> obtainConnection().");
 		return connectionOptional.get();
 	}
 }
