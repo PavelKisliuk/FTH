4729c33fb80b0b5fcb01f1af6f2e90deb01e8cd6
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-08-06 15:45:42 +0300

    PageRedirectionService logged

--- src/main/java/com/pavelkisliuk/fth/controller/pageservice/PageRedirectionService.java
+++ src/main/java/com/pavelkisliuk/fth/controller/pageservice/PageRedirectionService.java
@@ -8,6 +8,9 @@ import com.pavelkisliuk.fth.controller.FthService;
 import com.pavelkisliuk.fth.exception.FthControllerException;
 import com.pavelkisliuk.fth.model.FthString;
 import com.pavelkisliuk.fth.servlet.PageType;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +24,8 @@ import java.util.Map;
  * @since 12.0
  */
 public class PageRedirectionService implements FthService<FthString> {
+	private static final Logger LOGGER = LogManager.getLogger();
+
 	/**
 	 * Retrieve page for redirection as parameter and return it as JSON string.
 	 * <p>
@@ -31,6 +36,8 @@ public class PageRedirectionService implements FthService<FthString> {
 	 */
 	@Override
 	public String serve(FthString redirectionPage) throws FthControllerException {
+		LOGGER.log(Level.DEBUG,
+				"Start PageRedirectionService -> serve(FthString).");
 		if (redirectionPage == null) {
 			throw new FthControllerException(
 					"null parameter in PageRedirectionService -> serve(FthString)");
@@ -39,12 +46,16 @@ public class PageRedirectionService implements FthService<FthString> {
 		Map<String, String> responseJson = new HashMap<>();
 		try {
 			responseJson.put(KEY_REDIRECT, PageType.valueOf(redirectionPage.get()).get());
+			LOGGER.log(Level.INFO,
+					"Put page to responseJson.");
 		} catch (IllegalArgumentException e) {
 			throw new FthControllerException(
 					"Incorrect PageType in PageRedirectionService -> serve(FthString)");
 		}
 		// FIXME: 06.08.2019 убрать
 		//return new Gson().toJson(responseJson);
+		LOGGER.log(Level.DEBUG,
+				"Finish PageRedirectionService -> serve(FthString).");
 		return GSON.toJson(responseJson);
 	}
 }
