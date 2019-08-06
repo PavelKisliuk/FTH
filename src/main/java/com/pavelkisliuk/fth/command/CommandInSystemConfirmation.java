3c5b35f8574ab9dd7903bfd678bd829d702e308f
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-08-06 16:03:56 +0300

    CommandInSystemConfirmation commented

--- src/main/java/com/pavelkisliuk/fth/command/CommandInSystemConfirmation.java
+++ src/main/java/com/pavelkisliuk/fth/command/CommandInSystemConfirmation.java
@@ -1,6 +1,9 @@
+/*  By Pavel Kisliuk, 07.07.2019
+ *  This is class for education and nothing rights don't reserved.
+ */
+
 package com.pavelkisliuk.fth.command;
 
-import com.google.gson.Gson;
 import com.pavelkisliuk.fth.controller.pageservice.PageRedirectionService;
 import com.pavelkisliuk.fth.exception.FthCommandException;
 import com.pavelkisliuk.fth.exception.FthControllerException;
@@ -8,16 +11,43 @@ import com.pavelkisliuk.fth.model.FthString;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * The {@code CommandInSystemConfirmation} class is {@code FthServletCommand} realization for
+ * determination whether user in system.
+ * <p>
+ *
+ * @author Kisliuk Pavel Sergeevich
+ * @since 12.0
+ */
 public class CommandInSystemConfirmation implements FthServletCommand {
+	/**
+	 * Expected condition of session for further actions.
+	 * We can redirect to another page if already in system, or if not in system,
+	 * and condition designate it.
+	 * E.g. on authentication page we redirect to main page if session established,
+	 * but on main page we redirect if session NOT established.
+	 */
+	private static final String CONDITION = "condition";
+
+	/**
+	 * Determine condition of user in system and in terms of {@code CONDITION} decide
+	 * redirect his to another page or not.
+	 * <p>
+	 *
+	 * @param request is request from user.
+	 * @return redirect page if necessary, or return empty JSON string.
+	 * @throws FthCommandException if {@code FthControllerException} occurred.
+	 */
 	@Override
 	public String execute(HttpServletRequest request) throws FthCommandException {
-		FthString page = new CreatorString().create(request);
-		String message = "\"\"";
+		FthString redirectionPage = new CreatorString().create(request);
+		String message = EMPTY_JSON;
 		try {
-			boolean inSystemFlag = request.getSession().getAttribute(FthServletCommand.TRAINER_ID_ATTRIBUTE) != null;
-			boolean condition = Boolean.valueOf(request.getParameter("condition"));
+			//check is session already established for special ID
+			boolean inSystemFlag = request.getSession().getAttribute(ID_ATTRIBUTE) != null;
+			boolean condition = Boolean.parseBoolean(request.getParameter(CONDITION));
 			if (inSystemFlag == condition) {
-				message = new PageRedirectionService().serve(page);
+				message = new PageRedirectionService().serve(redirectionPage);
 			}
 		} catch (FthControllerException e) {
 			throw new FthCommandException(
