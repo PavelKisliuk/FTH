9f0177f051a263ae3e2a32530a5bc068fcd52869
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-08-06 15:38:36 +0300

    PageRedirectionService commented

--- src/main/java/com/pavelkisliuk/fth/controller/pageservice/PageRedirectionService.java
+++ src/main/java/com/pavelkisliuk/fth/controller/pageservice/PageRedirectionService.java
@@ -1,25 +1,49 @@
+/*  By Pavel Kisliuk, 07.07.2019
+ *  This is class for education and nothing rights don't reserved.
+ */
 package com.pavelkisliuk.fth.controller.pageservice;
 
-import com.google.gson.Gson;
 import com.pavelkisliuk.fth.controller.FthService;
 import com.pavelkisliuk.fth.exception.FthControllerException;
-import com.pavelkisliuk.fth.model.FthData;
 import com.pavelkisliuk.fth.model.FthString;
 import com.pavelkisliuk.fth.servlet.PageType;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class PageRedirectionService implements FthService {
+/**
+ * The {@code PageRedirectionService} class is {@code FthService} realization for
+ * designation page for redirection user.
+ * <p>
+ *
+ * @author Kisliuk Pavel Sergeevich
+ * @since 12.0
+ */
+public class PageRedirectionService implements FthService<FthString> {
+	/**
+	 * Retrieve page for redirection as parameter and return it as JSON string.
+	 * <p>
+	 *
+	 * @param redirectionPage is name of redirection page.
+	 * @return redirection page as JSON string.
+	 * @throws FthControllerException if retrieve null; {@code IllegalArgumentException} occurred.
+	 */
 	@Override
-	public String serve(FthData data) throws FthControllerException {
-		Map<String, String> responseJson = new HashMap<>();
-		FthString fthString = (FthString) data;
-		try {
-			responseJson.put("page", PageType.valueOf(fthString.get()).get());
-		} catch (IllegalArgumentException | NullPointerException e) {
-
+	public String serve(FthString redirectionPage) throws FthControllerException {
+		if (redirectionPage == null) {
+			throw new FthControllerException(
+					"null parameter in PageRedirectionService -> serve(FthString)");
 		}
-		return new Gson().toJson(responseJson);
+
+		Map<String, String> responseJson = new HashMap<>();
+		try {
+			responseJson.put(KEY_REDIRECT, PageType.valueOf(redirectionPage.get()).get());
+		} catch (IllegalArgumentException e) {
+			throw new FthControllerException(
+					"Incorrect PageType in PageRedirectionService -> serve(FthString)");
+		}
+		// FIXME: 06.08.2019 убрать
+		//return new Gson().toJson(responseJson);
+		return GSON.toJson(responseJson);
 	}
 }
