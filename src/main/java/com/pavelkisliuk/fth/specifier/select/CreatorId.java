76e6ce803b8ca6e06a5342f521fb107a9f67c1aa
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-08-07 11:52:13 +0300

    add CreatorId

---
+++ src/main/java/com/pavelkisliuk/fth/specifier/select/CreatorId.java
@@ -0,0 +1,24 @@
+package com.pavelkisliuk.fth.specifier.select;
+
+import com.pavelkisliuk.fth.exception.FthRepositoryException;
+import com.pavelkisliuk.fth.model.FthLong;
+import com.pavelkisliuk.fth.repository.FthDataByResultSetFactory;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CreatorId implements FthDataByResultSetFactory {
+	private static final int TRAINER_ID = 1;
+	private static final int CLIENT_ID = 2;
+
+	@Override
+	public FthLong create(ResultSet resultSet) throws FthRepositoryException {
+		try {
+			long trainerId = resultSet.getLong(TRAINER_ID);
+			long clientId = resultSet.getLong(CLIENT_ID);
+			return (trainerId != 0) ? new FthLong(trainerId) : new FthLong(clientId);
+		} catch (SQLException e) {
+			throw new FthRepositoryException("SQLException in CreatorLong -> create(ResultSet).", e);
+		}
+	}
+}
