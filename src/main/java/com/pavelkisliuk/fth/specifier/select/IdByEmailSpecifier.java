28645e95783d3bfa7e39b54525a4f971b190bcf0
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-08-07 11:51:44 +0300

    add IdByEmailSpecifier IdByEmailSpecifier commented

--- src/main/java/com/pavelkisliuk/fth/specifier/select/IdByEmailSpecifier.java
+++ src/main/java/com/pavelkisliuk/fth/specifier/select/IdByEmailSpecifier.java
@@ -1,3 +1,7 @@
+/*  By Pavel Kisliuk, 07.08.2019
+ *  This is class for education and nothing rights don't reserved.
+ */
+
 package com.pavelkisliuk.fth.specifier.select;
 
 import com.pavelkisliuk.fth.exception.FthRepositoryException;
@@ -7,23 +11,56 @@ import com.pavelkisliuk.fth.specifier.FthSelectSpecifier;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+/**
+ * The {@code IdByEmailSpecifier} class is {@code FthSelectSpecifier} realization
+ * for retrieving id of user by e-mail from AuthenticationData.
+ * <p>
+ *
+ * @author Kisliuk Pavel Sergeevich
+ * @since 12.0
+ */
 public class IdByEmailSpecifier implements FthSelectSpecifier {
+	/**
+	 * Select request to database.
+	 */
 	private static final String REQUEST = "SELECT " +
-			"personalID " +
+			"trainerId, clientId " +
 			"FROM AuthenticationData " +
 			"WHERE eMail = ?";
 
+	/**
+	 * E-mail of user.
+	 */
 	private FthString eMail;
 
+	/**
+	 * Constructor for fields initialization.
+	 * <p>
+	 *
+	 * @param eMail for {@code eMail} initialization.
+	 */
 	public IdByEmailSpecifier(FthString eMail) {
 		this.eMail = eMail;
 	}
 
+	/**
+	 * Return factory for {@code FthLong} creation.
+	 * <p>
+	 *
+	 * @return factory for {@code FthLong} creation.
+	 */
 	@Override
-	public CreatorLong createFactory() {
-		return new CreatorLong();
+	public CreatorId createFactory() {
+		return new CreatorId();
 	}
 
+	/**
+	 * Paste metadata in {@param statement} and return it.
+	 * <p>
+	 *
+	 * @param statement for pasting metadata into.
+	 * @return {@param statement}.
+	 */
 	@Override
 	public PreparedStatement pasteMeta(PreparedStatement statement) throws FthRepositoryException {
 		try {
@@ -35,6 +72,12 @@ public class IdByEmailSpecifier implements FthSelectSpecifier {
 		return statement;
 	}
 
+	/**
+	 * Return {@code REQUEST}.
+	 * <p>
+	 *
+	 * @return {@code REQUEST}.
+	 */
 	@Override
 	public String deriveSequelRequest() {
 		return REQUEST;
