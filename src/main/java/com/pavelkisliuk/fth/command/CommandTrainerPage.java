169e5e7db25a25a142af7cfbcf5f231de3f04ed7
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-08-07 12:57:52 +0300

    CommandTrainerPage commented

--- src/main/java/com/pavelkisliuk/fth/command/CommandTrainerPage.java
+++ src/main/java/com/pavelkisliuk/fth/command/CommandTrainerPage.java
@@ -1,3 +1,7 @@
+/*  By Pavel Kisliuk, 16.07.2019
+ *  This is class for education and nothing rights don't reserved.
+ */
+
 package com.pavelkisliuk.fth.command;
 
 import com.pavelkisliuk.fth.controller.pageservice.TrainerPageService;
@@ -7,11 +11,26 @@ import com.pavelkisliuk.fth.model.FthLong;
 
 import javax.servlet.http.HttpServletRequest;
 
-public class CommandTrainerPage implements FthServletCommand {
+/**
+ * The {@code CommandTrainerPage} class is {@code FthServletCommand} realization for
+ * trainer page data retrieving.
+ * <p>
+ *
+ * @author Kisliuk Pavel Sergeevich
+ * @since 12.0
+ */
+class CommandTrainerPage implements FthServletCommand {
+	/**
+	 * Obtain data for trainer page by trainer id.
+	 * <p>
+	 *
+	 * @param request is request from user.
+	 * @return data for trainer page.
+	 * @throws FthCommandException if {@code FthControllerException} occurred.
+	 */
 	@Override
 	public String execute(HttpServletRequest request) throws FthCommandException {
-		FthLong trainerId = (FthLong) request.getSession().getAttribute(FthServletCommand.TRAINER_ID_ATTRIBUTE);
-		String message = null;
+		FthLong trainerId = (FthLong) request.getSession().getAttribute(ID_ATTRIBUTE);
 		try {
 			return new TrainerPageService().serve(trainerId);
 		} catch (FthControllerException e) {
