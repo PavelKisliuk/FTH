67e098fc34a1749eabb6ebc841a06f2329dc93da
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-08-11 13:59:50 +0300

    add ClientRequestRemoverUpdateSpecifier ClientRequestRemoverUpdateSpecifier commented

---
+++ src/main/java/com/pavelkisliuk/fth/specifier/update/ClientRequestRemoverUpdateSpecifier.java
@@ -0,0 +1,74 @@
+/*  By Pavel Kisliuk, 11.08.2019
+ *  This is class for education and nothing rights don't reserved.
+ */
+
+package com.pavelkisliuk.fth.specifier.update;
+
+import com.pavelkisliuk.fth.exception.FthRepositoryException;
+import com.pavelkisliuk.fth.model.FthLong;
+import com.pavelkisliuk.fth.specifier.FthUpdateSpecifier;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * The {@code ClientRequestRemoverUpdateSpecifier} class is {@code FthUpdateSpecifier} realization for
+ * shifting in ClientPublicData table exerciseRequest condition.
+ * <p>
+ *
+ * @author Kisliuk Pavel Sergeevich
+ * @since 12.0
+ */
+public class ClientRequestRemoverUpdateSpecifier implements FthUpdateSpecifier {
+	/**
+	 * Update request to database.
+	 */
+	private static final String REQUEST = "UPDATE " +
+			"ClientPublicData " +
+			"SET exerciseRequest = false " +
+			"WHERE clientId = ?";
+
+	/**
+	 * ID of client.
+	 */
+	private FthLong clientId;
+
+	/**
+	 * Constructor for fields initialization.
+	 * <p>
+	 *
+	 * @param clientId for {@code clientId} initialization.
+	 */
+	public ClientRequestRemoverUpdateSpecifier(FthLong clientId) {
+		this.clientId = clientId;
+	}
+
+	/**
+	 * Paste metadata in {@code PreparedStatement} to update this data to database.
+	 * <p>
+	 *
+	 * @param statement for pasting metadata into.
+	 * @throws FthRepositoryException if {@code SQLException} occurred.
+	 */
+	@Override
+	public void update(PreparedStatement statement) throws FthRepositoryException {
+		try {
+			statement.setLong(1, clientId.get());
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			throw new FthRepositoryException(
+					"SQL exception in ClientRequestRemoverSpecifier -> update(PreparedStatement).", e);
+		}
+	}
+
+	/**
+	 * Return SQL request as String.
+	 * <p>
+	 *
+	 * @return SQL request for {@code PreparedStatement} as String.
+	 */
+	@Override
+	public String deriveSequelRequest() {
+		return REQUEST;
+	}
+}
