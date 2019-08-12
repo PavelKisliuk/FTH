632a36343292d052f6641a0a2bf74900fbdfe644
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-08-13 00:14:50 +0300

    add CreatorNewDrillBase

---
+++ src/main/java/com/pavelkisliuk/fth/command/CreatorNewDrillBase.java
@@ -0,0 +1,19 @@
+package com.pavelkisliuk.fth.command;
+
+import com.pavelkisliuk.fth.exception.FthCommandException;
+import com.pavelkisliuk.fth.model.FthDrillBase;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class CreatorNewDrillBase implements FthDataByRequestFactory {
+	private static final String DRILL_NAME = "name";
+	private static final String MUSCLE_GROUP_ID = "muscle";
+
+	@Override
+	public FthDrillBase create(HttpServletRequest request) throws FthCommandException {
+		FthDrillBase drillBase = new FthDrillBase();
+		drillBase.setDrillName(request.getParameter(DRILL_NAME));
+		drillBase.setMuscleGroupId(Long.parseLong(request.getParameter(MUSCLE_GROUP_ID)));
+		return drillBase;
+	}
+}
