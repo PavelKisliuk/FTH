d3d45a27d6c2c4d5d9e8d3f73da3294c5abf8329
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-08-14 16:23:33 +0300

    add TrainerRemoveClientService TrainerRemoveClientService commented

--- src/main/java/com/pavelkisliuk/fth/service/remove/TrainerRemoveClientService.java
+++ src/main/java/com/pavelkisliuk/fth/service/remove/TrainerRemoveClientService.java
@@ -2,63 +2,59 @@
  *  This is class for education and nothing rights don't reserved.
  */
 
-package com.pavelkisliuk.fth.controller.removeservice;
+package com.pavelkisliuk.fth.service.remove;
 
-import com.pavelkisliuk.fth.controller.FthService;
-import com.pavelkisliuk.fth.exception.FthControllerException;
+import com.pavelkisliuk.fth.command.FthServletCommand;
 import com.pavelkisliuk.fth.exception.FthRepositoryException;
-import com.pavelkisliuk.fth.model.FthData;
+import com.pavelkisliuk.fth.exception.FthServiceException;
 import com.pavelkisliuk.fth.model.FthLong;
 import com.pavelkisliuk.fth.repository.FthRepository;
-import com.pavelkisliuk.fth.specifier.update.TrainerRemoveClientSpecifier;
+import com.pavelkisliuk.fth.service.FthService;
+import com.pavelkisliuk.fth.specifier.update.TrainerRemoveClientUpdateSpecifier;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * The {@code TrainerRemoveClientService} is {@code FthService} realization to unchain
+ * The {@code TrainerRemoveClientService} class is {@code FthService} realization to unchain
  * conjunction between trainer and client.
  * <p>
  *
  * @author Kisliuk Pavel Sergeevich
  * @since 12.0
  */
-public class TrainerRemoveClientService implements FthService {
+public class TrainerRemoveClientService implements FthService<FthLong> {
 	private static final Logger LOGGER = LogManager.getLogger();
 
 	/**
 	 * Constrain database unchain conjunction between trainer and client.
 	 * <p>
 	 *
-	 * @param data is id of client in database to find his conjunction with trainer.
+	 * @param clientId is id of client in database to find his conjunction with trainer.
 	 * @return empty {@code String}.
-	 * @throws FthControllerException if {@code FthRepositoryException} occurred.
+	 * @throws FthServiceException if {@code FthRepositoryException} occurred.
 	 */
 	@Override
-	public String serve(FthData data) throws FthControllerException {
+	public String serve(FthLong clientId) throws FthServiceException {
 		LOGGER.log(Level.DEBUG,
 				"Start TrainerRemoveClientService -> serve(FthData).");
-		if (data == null ||
-				data.getClass() != FthLong.class) {
-			LOGGER.log(Level.ERROR,
-					"Incorrect parameter in TrainerRemoveClientService -> serve(FthData)!!!");
-			return "";
+		if (clientId == null) {
+			throw new FthServiceException(
+					"null parameter in TrainerRemoveClientService -> serve(FthLong).");
 		}
-
-		FthLong clientId = (FthLong) data;
 		try {
 			LOGGER.log(Level.INFO,
 					"Start unchaining.");
 			//Unchain.
-			FthRepository.INSTANCE.replace(new TrainerRemoveClientSpecifier(clientId));
+			FthRepository.INSTANCE.replace(new TrainerRemoveClientUpdateSpecifier(clientId));
 			LOGGER.log(Level.INFO,
 					"Unchained.");
 		} catch (FthRepositoryException e) {
-			throw new FthControllerException(
+			throw new FthServiceException(
 					"FthRepositoryException in TrainerRemoveClientService -> serve(FthData).", e);
 		}
 		LOGGER.log(Level.DEBUG,
 				"Finish TrainerRemoveClientService -> serve(FthData).");
-		return "";
+		return FthServletCommand.EMPTY_JSON;
 	}
 }
